171fcacc0dddade37e714a45c5502f20c0f3fb33
Jared <jared_glenn@yahoo.com>
2023-10-18 23:54:42 -0600

    commit up to step 6

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -214,7 +214,34 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    if not g.user:
+        flash("Please login first.")
+        return redirect("/")
+    
+    user_id = session[CURR_USER_KEY]
+    user = User.query.get_or_404(user_id)
+            
+    form = UserEditForm(obj=user)
+    
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data
+            user.header_image_url = form.header_image_url.data
+            user.bio = form.bio.data
+            
+            db.session.commit()
+            return redirect(f'/users/{user_id}')
+
+        else:
+            flash("Invalid credentials.", 'danger')
+            return redirect('/')
+
+    return render_template('users/edit.html', form=form, user=user)
 
 
 @app.route('/users/delete', methods=["POST"])
@@ -295,11 +322,19 @@ def homepage():
     """
 
     if g.user:
+        print(f"!!!!!!!!!!!!!!!!UsersFirst: {g.user.following} !!!!!!!!!!!!!")
+        users = [g.user.id]
+        for user in g.user.following:
+            users.append(user.id)
+        print(f"!!!!!!!!!!!!!!!!UsersSecond: {users} !!!!!!!!!!!!!")
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(users))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
+        
+        print(f"!!!!!!!!!!!!!!!Messages: {messages} !!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
         return render_template('home.html', messages=messages)
 
